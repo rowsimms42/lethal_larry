43b96988b431b9e13e3fa952086fb2ce72c2c247
Rowan <>
2021-02-08 18:58:45 -0500

    pushing

---
+++ LethalLarry/Assets/Scripts/menuscript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menuscript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    public void directionPage(){
+      Debug.Log("Going to directions...");
+      SceneManager.LoadScene("Directions");
+    }
+
+    public void playGame(){
+      Debug.Log("Starting game...");
+      SceneManager.LoadScene("SampleScene");
+    }
+
+    public void exitGame(){
+      Debug.Log("Exiting...");
+      Application.Quit();
+    }
+}
---
+++ LethalLarry/Assets/Scripts/playerScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using System;
+
+public class playerScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Rigidbody2D body;
+    Animator animator;
+
+    float horizontal;
+    float vertical;
+
+    public float runSpeed = 20.0f;
+
+    void Start ()
+    {
+      body = GetComponent<Rigidbody2D>();
+      animator = GetComponent<Animator> ();
+
+    }
+
+    void Update()
+    {
+      horizontal = Input.GetAxisRaw("Horizontal"); // -1 is left
+      vertical = Input.GetAxisRaw("Vertical"); // -1 is down
+    }
+
+    void FixedUpdate()
+    {
+      body.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
+    }
+
+}
