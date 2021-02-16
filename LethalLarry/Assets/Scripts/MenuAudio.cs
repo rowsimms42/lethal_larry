df77a2b00f7877a324d9a80f1e0c384016ef553b
salazjos <56235858+salazjos@users.noreply.github.com>
2021-02-15 21:25:12 -0700

    feb 15 additions
    added boundary collision, level 1 menu music, stars, menu music.

---
+++ LethalLarry/Assets/Scripts/MenuAudio.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuAudio : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SceneManager.sceneLoaded += destroyGameObject;
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    void destroyGameObject(Scene s, LoadSceneMode lsm)
+    {
+        if(s.name == "lev1")
+        {
+            SceneManager.sceneLoaded -= destroyGameObject;
+            Destroy(this.gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
